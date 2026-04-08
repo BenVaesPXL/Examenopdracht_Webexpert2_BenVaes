9c50dc51931c446deaaba1a4fb76794607c7535c
BenVaesPXL <12100455@student.pxl.be>
2026-04-08 11:16:11 +0200

    added drawer navigation

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import 'react-native-gesture-handler';
 import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import HomeScreen from './app/screens/HomeScreen';
 import ScanScreen from './app/screens/ScanScreen';
@@ -9,13 +12,11 @@ import RoomListScreen from './app/screens/RoomListScreen';
 import RoomDetailScreen from './app/screens/RoomDetailScreen';
 import ProfielScreen from './app/screens/ProfielScreen';
 import ReportFormScreen from './app/screens/ReportFormScreen';
+import SettingsScreen from './app/screens/SettingsScreen';
 
 const LokaalenStack = createNativeStackNavigator({
     screens: {
-        RoomList: {
-            screen: RoomListScreen,
-            options: { headerShown: false },
-        },
+        RoomList: RoomListScreen,
         RoomDetail: RoomDetailScreen,
     },
 });
@@ -31,7 +32,7 @@ const ProfielStack = createNativeStackNavigator({
 });
 
 const Tabs = createBottomTabNavigator({
-    screenOptions:{
+    screenOptions: {
         headerShown: false,
     },
     screens: {
@@ -48,21 +49,27 @@ const Tabs = createBottomTabNavigator({
     },
 });
 
-const RootStack = createNativeStackNavigator({
+const AppDrawer = createDrawerNavigator({
+    screenOptions: {
+        drawerType: 'front',
+    },
     screens: {
         Main: {
             screen: Tabs,
             options: { headerShown: false },
         },
+        Settings: SettingsScreen,
     },
 });
 
-const Navigation = createStaticNavigation(RootStack);
+const Navigation = createStaticNavigation(AppDrawer);
 
 export default function App() {
     return (
-        <SafeAreaProvider>
-            <Navigation />
-        </SafeAreaProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <SafeAreaProvider>
+                <Navigation />
+            </SafeAreaProvider>
+        </GestureHandlerRootView>
     );
 }
